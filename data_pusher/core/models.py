c54e8e05373dc133c8f5c6814cbceed2aee2a86e
Arsh Bhatia <71868070+arshbhatia8@users.noreply.github.com>
2024-05-25 00:17:57 +0530

    Create models.py

---
+++ data_pusher/core/models.py
@@ -0,0 +1,23 @@
+# core/models.py
+
+from django.db import models
+import uuid
+
+class Account(models.Model):
+    email = models.EmailField(unique=True)
+    account_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    account_name = models.CharField(max_length=255)
+    app_secret_token = models.CharField(max_length=255, unique=True, default=uuid.uuid4)
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.account_name
+
+class Destination(models.Model):
+    account = models.ForeignKey(Account, related_name='destinations', on_delete=models.CASCADE)
+    url = models.URLField()
+    http_method = models.CharField(max_length=10, choices=[('GET', 'GET'), ('POST', 'POST'), ('PUT', 'PUT')])
+    headers = models.JSONField()
+
+    def __str__(self):
+        return self.url
